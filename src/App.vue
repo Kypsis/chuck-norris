fbdb1424726a33faa36173293e51fc0ba65fe366
Harry Sild <puhhbaer@hotmail.com>
2020-03-07 20:30:43 +0200

    initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-card>
+      <v-toolbar color="cyan" dark flat>
+        <v-toolbar-title>Chuck Norris Facts</v-toolbar-title>
+
+        <template v-slot:extension>
+          <v-tabs
+            v-model="model"
+            center-active
+            show-arrows
+            slider-color="yellow"
+          >
+            <v-tab
+              v-for="category in categories"
+              :key="category"
+              :href="`#tab-${category}`"
+            >
+              {{ category }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+
+      <v-tabs-items v-model="model">
+        <v-tab-item
+          v-for="category in categories"
+          :key="category"
+          :value="`tab-${category}`"
+        >
+          <v-card flat>
+            <v-card-text v-text="text"></v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  created() {
+    this.$store.dispatch("getCategories");
+  },
+  components: {},
+  computed: {
+    categories: function() {
+      return this.$store.state.categories;
+    }
+  },
+  data() {
+    return {
+      model: "tabs",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    };
+  },
+  props: {
+    source: String
+  },
+  methods: {
+    logCategories: function() {
+      console.log(this.categories);
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    categories: []
   },
   mutations: {
+    getCategories(state, payload) {
+      state.categories = payload;
+    }
   },
   actions: {
+    getCategories(context) {
+      axios
+        .get(`${process.env.VUE_APP_BASEURL}/categories`)
+        .then(response => {
+          context.commit("getCategories", response.data);
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
